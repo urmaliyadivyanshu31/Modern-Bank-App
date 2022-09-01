753620b0a29ce1266f4c631aa3fb2fa76aefe378
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2022-09-01 21:26:55 +0530

    Modern Banking App
    Modern banking app interface using React JS and Tailwind CSS

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        You can only find better deals at this site that will help you in your
+        functional day-to-day life.
+      </p>
+
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
